69216b918d63a83307ef866dea4870b72bf6805a
itzkowitch <46599062+itzkowitch@users.noreply.github.com>
2021-04-02 11:45:54 +0200

    Add files via upload

--- dialogs/rootDialog.js
+++ dialogs/rootDialog.js
@@ -25,23 +25,32 @@ class RootDialog extends ComponentDialog {
 
         // Set up a series of questions for collecting the user's name.
         const fullnameSlots = [
-            new SlotDetails('first', 'text', 'Please enter your first name.'),
-            new SlotDetails('last', 'text', 'Please enter your last name.')
+
+            new SlotDetails('first', 'text', 'Bienvenue sur notre site de commande en ligne. Quel est votre prenom ?'),
+            new SlotDetails('last', 'text', 'Entrez votre nom de famille svp.')
+        ];
+        const auguste = [
+            new SlotDetails('coucou', 'text', 'coucou.'),
+            new SlotDetails('sss', 'text', 'sss')
         ];
 
         // Set up a series of questions to collect a street address.
         const addressSlots = [
-            new SlotDetails('street', 'text', 'Please enter your street address.'),
-            new SlotDetails('city', 'text', 'Please enter the city.'),
-            new SlotDetails('zip', 'text', 'Please enter your zipcode.')
+            new SlotDetails('street', 'text', 'Quelle est votre adresse postale ?'),
+            new SlotDetails('city', 'text', 'Dans quelle ville ?'),
+            new SlotDetails('zip', 'text', 'Entrez votre code postal svp')
         ];
 
         // Link the questions together into a parent group that contains references
         // to both the fullname and address questions defined above.
         const slots = [
+           // new SlotDetails('aug', 'aug'),
             new SlotDetails('fullname', 'fullname'),
-            new SlotDetails('age', 'number', 'Please enter your age.'),
-            new SlotDetails('shoesize', 'shoesize', 'Please enter your shoe size.', 'You must enter a size between 0 and 16. Half sizes are acceptable.'),
+            new SlotDetails('age', 'number', 'Entrez votre age svp'),
+            new SlotDetails('shoesize', 'shoesize', 'Quelle est votre pointure', 'Nous n avons les tailles qu entre 36 et 48. Les demi tailles sont acceptes.'),
+            new SlotDetails('shoename', 'shoename', 'Nous avons seulement yeezy (1) et jordan (2) en stock. Que souhaitez vous ?', 'You must enter only 1 or 2'),
+            new SlotDetails('color', 'color', 'Nous avons du bleu (1) ou du vert (2). Lesquelles souhaitez vous?', 'You must enter only 1 or 2'),
+
             new SlotDetails('address', 'address')
         ];
 
@@ -50,15 +59,21 @@ class RootDialog extends ComponentDialog {
         // because they are both based on the provided Dialog class.
         this.addDialog(new SlotFillingDialog('address', addressSlots));
         this.addDialog(new SlotFillingDialog('fullname', fullnameSlots));
+        this.addDialog(new SlotFillingDialog('aug', auguste));
         this.addDialog(new TextPrompt('text'));
         this.addDialog(new NumberPrompt('number'));
         this.addDialog(new NumberPrompt('shoesize', this.shoeSizeValidator));
+        this.addDialog(new NumberPrompt('shoename', this.shoeNameValidator));
+        this.addDialog(new NumberPrompt('color', this.colorValidator));
+
+
         this.addDialog(new SlotFillingDialog('slot-dialog', slots));
 
         // Finally, add a 2-step WaterfallDialog that will initiate the SlotFillingDialog,
         // and then collect and display the results.
         this.addDialog(new WaterfallDialog('root', [
             this.startDialog.bind(this),
+           // this.process.bind(this),
             this.processResults.bind(this)
         ]));
 
@@ -88,6 +103,31 @@ class RootDialog extends ComponentDialog {
         return await step.beginDialog('slot-dialog');
     }
 
+        async process(step) {
+        // Each "slot" in the SlotFillingDialog is represented by a field in step.result.values.
+        // The complex that contain subfields have their own .values field containing the sub-values.
+        const values = step.result.values;
+
+        //await step.context.sendActivity(`You wear a size ${ values.shoesize } shoes.`);
+        if (values.shoename == 1) {
+            if (values.color == 1) {
+                await step.context.sendActivity(`vous avez dans votre panier des  yeezy bleues en taille ${ values.shoesize }.`);
+            }   
+            if (values.color == 2) {
+                await step.context.sendActivity(`vous avez dans votre panier des  yeezy vertes en taille ${ values.shoesize }.`);
+            }
+        }
+        if (values.shoename == 2) {
+            if (values.color == 1) {
+                 await step.context.sendActivity(`vous avez dans votre panier  des  jordan bleues en taille ${ values.shoesize }.`);
+            } 
+            if (values.color == 2) {
+                 await step.context.sendActivity(`vous avez dans votre panier  des  jordan vertes en taille ${ values.shoesize }.`);
+            }
+        }
+    }
+
+
     // This is the second step of the WaterfallDialog.
     // It receives the results of the SlotFillingDialog and displays them.
     async processResults(step) {
@@ -96,12 +136,28 @@ class RootDialog extends ComponentDialog {
         const values = step.result.values;
 
         const fullname = values.fullname.values;
-        await step.context.sendActivity(`Your name is ${ fullname.first } ${ fullname.last }.`);
+        await step.context.sendActivity(`Merci  ${ fullname.first } ${ fullname.last } pour ta commande. Elle sera expedie dans un delais de 3 jours ouvres`);
 
-        await step.context.sendActivity(`You wear a size ${ values.shoesize } shoes.`);
+        //await step.context.sendActivity(`You wear a size ${ values.shoesize } shoes.`);
+        if (values.shoename == 1) {
+            if (values.color == 1) {
+                await step.context.sendActivity(`vous avez commande des  yeezy bleues en taille ${ values.shoesize }.`);
+            }   
+            if (values.color == 2) {
+                await step.context.sendActivity(`vous avez commande des  yeezy vertes en taille ${ values.shoesize }.`);
+            }
+        }
+        if (values.shoename == 2) {
+            if (values.color == 1) {
+                 await step.context.sendActivity(`vous avez commande des  jordan bleues en taille ${ values.shoesize }.`);
+            } 
+            if (values.color == 2) {
+                 await step.context.sendActivity(`vous avez commande des  jordan vertes en taille ${ values.shoesize }.`);
+            }
+        }
 
         const address = values.address.values;
-        await step.context.sendActivity(`Your address is: ${ address.street }, ${ address.city } ${ address.zip }`);
+        await step.context.sendActivity(`Elles seront livrees a ton adresse : ${ address.street }, ${ address.city } ${ address.zip }`);
 
         return await step.endDialog();
     }
@@ -113,12 +169,36 @@ class RootDialog extends ComponentDialog {
             const shoesize = prompt.recognized.value;
 
             // Shoe sizes can range from 0 to 16.
-            if (shoesize >= 0 && shoesize <= 16) {
+            if (shoesize >= 36 && shoesize <= 48) {
                 // We only accept round numbers or half sizes.
                 if (Math.floor(shoesize) === shoesize || Math.floor(shoesize * 2) === shoesize * 2) {
                     // Indicate success.
                     return true;
                 }
+            }
+        }
+
+        return false;
+    }
+    async shoeNameValidator(prompt) {
+        if (prompt.recognized.succeeded) {
+            const shoename = prompt.recognized.value;
+
+            // Shoe can be yeezy or jordan
+            if (shoename == 1 || shoename == 2) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+    async colorValidator(prompt) {
+        if (prompt.recognized.succeeded) {
+            const color = prompt.recognized.value;
+
+            // Shoe can be yeezy or jordan
+            if (color == 1 || color == 2) {
+                return true;
             }
         }
 
